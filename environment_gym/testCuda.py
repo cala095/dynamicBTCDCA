b4714617870b1d5c8484fc1e423703a23ec7f549
Lox <cala95@gmail.com>
2024-12-04 13:29:18 +0100

    util for cuda drivers (at the moment for ppo the cpu looks better...)

---
+++ environment_gym/testCuda.py
@@ -0,0 +1,6 @@
+import torch
+
+print("Is CUDA available?:", torch.cuda.is_available())
+print("CUDA device count:", torch.cuda.device_count())
+print("Current CUDA device:", torch.cuda.current_device())
+print("Device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
