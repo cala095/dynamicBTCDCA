99d88a239c5a8a40f30d00d247a639ea0a374224
Lox <cala95@gmail.com>
2024-12-05 20:39:34 +0100

    removed the wrong clipping

--- environment_gym/PPOagentCreation.py
+++ environment_gym/PPOagentCreation.py
@@ -19,13 +19,13 @@ def main():
     data_1D = pd.read_csv('data/merged_data_1D.csv')
 
     # Number of environments to run in parallel
-    num_envs = 1  # Adjust based on your CPU cores
+    num_envs = 6  # Adjust based on your CPU cores
 
     # Create the vectorized environment
     env = SubprocVecEnv([make_env(data_1m, data_1H, data_1D) for _ in range(num_envs)])
 
     # Wrap the environment with VecNormalize # a good idea to avoid overfitting on normalizing the whole dataset?  i think yes because btc will be the one changing the most 
-    env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.0)
+    env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=1.0) #TODO maybe implement positive rewards? instead of negative ones?
 
     # Define a more powerful neural network architecture
     policy_kwargs = dict(
@@ -33,8 +33,8 @@ def main():
     )
 
     # Specify total timesteps
-    total_timesteps = 100000  # Adjust as needed
-
+    total_timesteps = 20000000  # Adjust as needed
+    #n_steps=43200 + total_timesteps = 10 000 000 -> 499s per total_timesteps | 259200 -> 5,34h
     # Create the PPO model with adjusted hyperparameters
     model = PPO(
         'MlpPolicy',
@@ -43,8 +43,8 @@ def main():
         device='cpu',
         n_steps=2048,
         batch_size=1024,
-        learning_rate =3e-4, #standard 0.0003
-        n_epochs=20,
+        learning_rate =0.0003, #standard 0.0003
+        n_epochs=20, #standard 20
         policy_kwargs=policy_kwargs,
         clip_range=0.2,
         ent_coef=0.01
